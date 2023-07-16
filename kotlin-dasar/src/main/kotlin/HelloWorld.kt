f3c872620ba6323bd9e7ec693375ba33c66ee5bd
kyohei7 <rizkimuhammad2301@gmail.com>
2023-07-16 22:04:44 +0700

    Init repo and learn hello world

---
+++ kotlin-dasar/src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,3 @@
+fun main() {
+    println("Android Developer- 2023 🐤")
+}
---
+++ kotlin-dasar/src/main/kotlin/TipeDataNumber.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    // Integer Number
+    var age: Byte = 26
+    var height: Int = 200
+    var distance: Short = 3000
+    var balance: Long = 1000000000L
+
+    // Floating Point Number
+    var value: Float = 98.75F
+    var radius: Double = 234234234.4545
+    
+}
